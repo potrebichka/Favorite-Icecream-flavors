815f07e2c48cf4f2ef4e562d732c60369704fc34
Nina Potrebich and Trevor Mackin <trevor@>
2019-10-28 11:24:01 -0700

    append elements to unordered list from JavaScript side to html

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
+  var flavors = ["Vanilla", "Chocolate", "Butter-pecan", "Strawberry", "Raspberry", "Mango"];
 
-
+  flavors.forEach(function(flavor) {
+    $("#list").append("<li>" + flavor + "</li>");
+  });
 
 });
